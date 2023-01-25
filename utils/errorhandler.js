17b87eba81c5b031cbfd12bc2be643148ffc9593
vikas harge <harge.vikas@gmail.com>
2023-01-25 15:39:01 +0530

    to be hosted on vercel

--- utils/errorhandler.js
+++ utils/errorhandler.js
@@ -1,15 +1,12 @@
-class ErrorHandler extends Error {
-    constructor(message, statusCode){
-         super(message);
+class ErrorHandler extends Error{
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode = statusCode
 
-         this.name = message
-         this.statusCode = statusCode;
-
-         Error.captureStackTrace(this, this.constructor)
-
+        Error.captureStackTrace(this,this.constructor);
 
     }
-
+    
 }
 
-module.exports = ErrorHandler;
+module.exports = ErrorHandler
